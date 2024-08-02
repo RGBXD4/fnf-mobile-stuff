06fe3d35709dc92c7b6e8cc3d7e6989e850f11a7
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-08-02 21:43:20 +0800

    updateing

--- stuffs/android/hitbox space type/1/ps.hx
+++ stuffs/android/hitbox space type/1/ps.hx
@@ -1,1 +1,26 @@
+if (noob)
+		{ 
+		switch (SONG.song.toLowerCase())
+		{
+		case 'dick':
+		#if android
+		addVirtualPad(NONE, MB);
+		addPadCamera();
+		addAndroidControls();
+		androidc.visible = true;	
+		#end 
 
+		default:
+		#if android
+		addAndroidControls();
+		androidc.visible = true;
+		#end
+		}
+		}
+		else
+		{
+		#if android
+		addAndroidControls();
+		androidc.visible = true;
+		#end
+    }
